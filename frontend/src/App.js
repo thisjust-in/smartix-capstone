cd2a5985cae9990374432595e5293dab803f00e2
Justin <sunnysidedigitals@gmail.com>
2021-06-30 14:47:12 +0800

    navbar-inprogress

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
 import MetaMaskBtn from "./components/MetaMaskBtn";
+import NavBar from "./components/Main-Components/NavBar";
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <MetaMaskBtn />
     </div>
   );
