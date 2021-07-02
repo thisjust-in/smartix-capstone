9bb1545026c1bff43b8db581d0f98cbe577ef99a
Karl-Hui <KarlHui0311@gmail.com>
2021-07-02 12:51:11 +0800

    addd

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import walletReducer from "./MetaMaskSlice";
+import eventCardReducer from "./EventCardSlice";
 
 export default configureStore({
   reducer: {
     wallet: walletReducer,
+    eventCard: eventCardReducer,
   },
 });
