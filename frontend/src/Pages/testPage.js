3f320ffe9aa904f27733ede0eb94fa3c48ebad7c
Justin <sunnysidedigitals@gmail.com>
2021-07-14 14:25:09 +0800

    jh branch

--- frontend/src/Pages/testPage.js
+++ frontend/src/Pages/testPage.js
@@ -4,25 +4,26 @@ import { useEffect } from "react";
 import web3 from "../web3";
 
 function Test() {
- 
-    let ContractCount
-    useEffect(async () => {
-        const accounts = await web3.eth.getAccounts();
-        ContractCount = await EventContract.methods.getContractCount().send({from: accounts[0]})
-    }, [])
+  let ContractCount;
+  // useEffect(async () => {
+  //   const accounts = await web3.eth.getAccounts();
+  //   ContractCount = await EventContract.methods
+  //     .getContractCount()
+  //     .send({ from: accounts[0] });
+  // }, []);
 
-
-    async function click() {
-        const accounts = await web3.eth.getAccounts();
-        let createEvent = await EventContract.methods.newEvent().send({from: accounts[0]})
-        console.log(createEvent)
-
-    }
+  async function click() {
+    const accounts = await web3.eth.getAccounts();
+    let createEvent = await EventContract.methods
+      .newEvent()
+      .send({ from: accounts[0] });
+    console.log(createEvent, "123");
+  }
 
   return (
     <div>
-     <button onClick={click}>Create Contract</button>
-     <p>{ContractCount} ttt</p>
+      <button onClick={click}>Create Contract</button>
+      <p>{ContractCount} ttt</p>
     </div>
   );
 }
