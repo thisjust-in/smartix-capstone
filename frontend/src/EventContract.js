ba87f0bf037d0a72f8fbdcd36f642c8f2d2dc299
Karl-Hui <KarlHui0311@gmail.com>
2021-07-23 11:28:00 +0800

    pulled from main

--- frontend/src/EventContract.js
+++ frontend/src/EventContract.js
@@ -1,194 +1,252 @@
-import web3 from './web3'
+import web3 from "./web3";
 
-const address = '0x09c189398848BD42cc7707947f7D7feE12DBB96E';
+const address = "0x09c189398848BD42cc7707947f7D7feE12DBB96E";
 
-const abi = [{
-  "anonymous": false,
-  "inputs": [{
-    "indexed": false,
-    "internalType": "address",
-    "name": "_eventaddress",
-    "type": "address"
-  }],
-  "name": "GetAddress",
-  "type": "event"
-}, {
-  "inputs": [{
-    "internalType": "address",
-    "name": "_eventaddress",
-    "type": "address"
-  }],
-  "name": "GetTixTypeCount",
-  "outputs": [{
-    "internalType": "uint256",
-    "name": "",
-    "type": "uint256"
-  }],
-  "stateMutability": "view",
-  "type": "function"
-}, {
-  "inputs": [{
-    "internalType": "address",
-    "name": "_eventaddress",
-    "type": "address"
-  }, {
-    "internalType": "uint256",
-    "name": "_amount",
-    "type": "uint256"
-  }],
-  "name": "Mint",
-  "outputs": [],
-  "stateMutability": "nonpayable",
-  "type": "function"
-}, {
-  "inputs": [{
-    "internalType": "address",
-    "name": "_eventaddress",
-    "type": "address"
-  }, {
-    "internalType": "uint256",
-    "name": "_tixID",
-    "type": "uint256"
-  }],
-  "name": "QtyPerTixType",
-  "outputs": [{
-    "internalType": "uint256",
-    "name": "",
-    "type": "uint256"
-  }],
-  "stateMutability": "view",
-  "type": "function"
-}, {
-  "inputs": [{
-    "internalType": "address",
-    "name": "_eventaddress",
-    "type": "address"
-  }, {
-    "internalType": "uint256",
-    "name": "_tixID",
-    "type": "uint256"
-  }],
-  "name": "TixPrice",
-  "outputs": [{
-    "internalType": "uint256",
-    "name": "",
-    "type": "uint256"
-  }],
-  "stateMutability": "view",
-  "type": "function"
-}, {
-  "inputs": [{
-    "internalType": "address",
-    "name": "_eventaddress",
-    "type": "address"
-  }, {
-    "internalType": "address",
-    "name": "_owner",
-    "type": "address"
-  }, {
-    "internalType": "uint256",
-    "name": "_tixID",
-    "type": "uint256"
-  }],
-  "name": "TixQtyPerUser",
-  "outputs": [{
-    "internalType": "uint256",
-    "name": "",
-    "type": "uint256"
-  }],
-  "stateMutability": "view",
-  "type": "function"
-}, {
-  "inputs": [{
-    "internalType": "address",
-    "name": "_eventaddress",
-    "type": "address"
-  }, {
-    "internalType": "uint256",
-    "name": "_tixID",
-    "type": "uint256"
-  }, {
-    "internalType": "uint256",
-    "name": "_qty",
-    "type": "uint256"
-  }],
-  "name": "buyTicket",
-  "outputs": [],
-  "stateMutability": "payable",
-  "type": "function"
-}, {
-  "inputs": [{
-    "internalType": "uint256",
-    "name": "",
-    "type": "uint256"
-  }],
-  "name": "contracts",
-  "outputs": [{
-    "internalType": "contract Event",
-    "name": "",
-    "type": "address"
-  }],
-  "stateMutability": "view",
-  "type": "function"
-}, {
-  "inputs": [],
-  "name": "eventID",
-  "outputs": [{
-    "internalType": "uint256",
-    "name": "",
-    "type": "uint256"
-  }],
-  "stateMutability": "view",
-  "type": "function"
-}, {
-  "inputs": [{
-    "internalType": "address",
-    "name": "",
-    "type": "address"
-  }],
-  "name": "eventLog",
-  "outputs": [{
-    "internalType": "address payable",
-    "name": "",
-    "type": "address"
-  }],
-  "stateMutability": "view",
-  "type": "function"
-}, {
-  "inputs": [],
-  "name": "getContractCount",
-  "outputs": [{
-    "internalType": "uint256",
-    "name": "contractCount",
-    "type": "uint256"
-  }],
-  "stateMutability": "view",
-  "type": "function"
-}, {
-  "inputs": [],
-  "name": "newEvent",
-  "outputs": [],
-  "stateMutability": "nonpayable",
-  "type": "function"
-}, {
-  "inputs": [{
-    "internalType": "address",
-    "name": "_eventaddress",
-    "type": "address"
-  }, {
-    "internalType": "uint256",
-    "name": "_tixID",
-    "type": "uint256"
-  }, {
-    "internalType": "uint256",
-    "name": "_price",
-    "type": "uint256"
-  }],
-  "name": "setPrice",
-  "outputs": [],
-  "stateMutability": "nonpayable",
-  "type": "function"
-}]
+const abi = [
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "address",
+        name: "_eventaddress",
+        type: "address",
+      },
+    ],
+    name: "GetAddress",
+    type: "event",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_eventaddress",
+        type: "address",
+      },
+    ],
+    name: "GetTixTypeCount",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_eventaddress",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "_amount",
+        type: "uint256",
+      },
+    ],
+    name: "Mint",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_eventaddress",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "_tixID",
+        type: "uint256",
+      },
+    ],
+    name: "QtyPerTixType",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_eventaddress",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "_tixID",
+        type: "uint256",
+      },
+    ],
+    name: "TixPrice",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_eventaddress",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "_owner",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "_tixID",
+        type: "uint256",
+      },
+    ],
+    name: "TixQtyPerUser",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_eventaddress",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "_tixID",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "_qty",
+        type: "uint256",
+      },
+    ],
+    name: "buyTicket",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    name: "contracts",
+    outputs: [
+      {
+        internalType: "contract Event",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "eventID",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    name: "eventLog",
+    outputs: [
+      {
+        internalType: "address payable",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getContractCount",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "contractCount",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "newEvent",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_eventaddress",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "_tixID",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "_price",
+        type: "uint256",
+      },
+    ],
+    name: "setPrice",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
 
-
-export default new web3.eth.Contract(abi, address)
+export default new web3.eth.Contract(abi, address);
