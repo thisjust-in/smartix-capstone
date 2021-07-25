de1b538926dedfb6f6c4873dbacf3ca76f178e6f
cheungchikting <cheungchikting@gmail.com>
2021-07-25 17:48:21 +0800

    jc

--- frontend/src/Pages/Loading.js
+++ frontend/src/Pages/Loading.js
@@ -1,9 +1,18 @@
 import React, { useState } from "react";
 import styles from "./Loading.module.css";
-import gif from "../components/assets/loading.gif";
 
 function Loading() {
-  return <div className={styles.gifcontainer} style={{"background-image": url();}}></div>;
+  return (
+    <div
+      className={styles.gifcontainer}
+      style={{ backgroundImage: `url("/giphy.gif")` }}
+    >
+      <h2 style={{ color: "white" }}>
+        We are working hard on the blockchain and it will take sometime,
+      </h2>
+      <h2 style={{ color: "white" }}>thank you for being patient with us!</h2>
+    </div>
+  );
 }
 
 export default Loading;
